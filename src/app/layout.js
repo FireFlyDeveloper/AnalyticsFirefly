0dba9b34503224ccd65aa1a43c10442097ace399
Kim <saludeskimdev@gmail.com>
2025-09-27 01:37:13 +0800

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className="bg-gradient-to-br from-blue-50 to-indigo-100 min-h-screen">{children}</body>
     </html>
   );
