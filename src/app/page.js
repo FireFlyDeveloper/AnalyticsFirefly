71f5d671b1ed7c07592ef5bda74a9f592c07cb7d
Kim <saludeskimdev@gmail.com>
2025-09-28 14:00:11 +0800

    structured

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import { FooterView } from "./footer";
-import { HeaderView } from "./header";
-import { MainView } from "./main";
+import { FooterView } from "../../components/layout/dashboard/footer";
+import { HeaderView } from "../../components/layout/dashboard/header";
+import { MainView } from "../../components/layout/dashboard/main";
 
 export default function Home() {
   return (
